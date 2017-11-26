c9bef2293d194ab87374d5c690a56f2629275bf9
tknr <info@tknr.com>
2017-11-27 01:08:50 +0900

    2017年 11月 27日 月曜日 01:08:50 JST

--- upconvert_mp3.sh
+++ upconvert_mp3.sh
@@ -9,20 +9,23 @@ if [ $# -eq 0 ]; then
 	exit 1
 fi
 
+## configuration
 TMP_DIR=/var/tmp/crond
+THRESHOLD_BITRATE=160
 TARGET_BITRATE=320
 TARGET_SAMPLING_RATE=48000
+##
+
 
 for TARGET_DIR in ${argv}
 do
 	for FILENAME in `find "${TARGET_DIR}" -name "*.mp3" | sort`
 	do
-		BITRATE=`soxi -B "${FILENAME}" | cut -d "k" -f 1`
+		BITRATE=`soxi -B "${FILENAME}" | cut -d "k" -f 1 | cut -d "." -f 1`
 		echo ''$FILENAME' : '$BITRATE' k'
-		DIFF=$(( TARGET_BITRATE - BITRATE ))
+		DIFF=$(( THRESHOLD_BITRATE - BITRATE ))
 		if [ $(( DIFF )) -gt 1 ]  ; then
-			soxi "${FILENAME}"
-			echo "under target bitrate. upconverting..."
+			echo "under threshold bitrate : ${THRESHOLD_BITRATE} . upconverting..."
 			serial=`uuidgen`
 
 			sox -S -G "${FILENAME}" -C ${TARGET_BITRATE} -r ${TARGET_SAMPLING_RATE} ${TMP_DIR}/${serial}.mp3 | continue
@@ -34,3 +37,4 @@ do
 		fi
 	done
 done
+rm ${TMP_DIR}/*.mp3
